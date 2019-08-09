90804b4ce580e93be26a57339f2f0cf0f513c34f
Michael Araya <michaelarayamurcia@gmail.com>
2019-08-08 18:04:53 -0600

    Colocando imagenes

--- Space Invaders/invaders.py
+++ Space Invaders/invaders.py
@@ -11,13 +11,32 @@ class Juego:
         self.negro = (0,0,0)
         self.blanco = (255,255,255)
         self.azul = (0,0,255)
-        self.speed = 1   
-
+        self.speed = 1  
+        self.img = pygame.image.load('purple.png') 
+    def show(self):
+        self.img = pygame.transform.scale(self.img,(self.largo,self.ancho))
+        self.screen.blit(self.img,(0,0))
+class Jugador:
+    def __init__(self):
+        self.x = juego.largo / 2
+        self.y = juego.ancho - 20
+        self.largo = 20
+        self.ancho = 20
+        self.dx = 0
+        self.img = pygame.image.load('playerShip1_blue.png')
+    def show(self):
+        self.img = pygame.transform.scale(self.img,(self.largo,self.ancho))
+        juego.screen.blit(self.img,(self.x,self.y))
 juego = Juego()
+jugador = Jugador()
+def update():
+    juego.show()
+    jugador.show()
+    pygame.display.update()
 
 while juego.gameover != True:
     for event in pygame.event.get():
 	#------------- Cerrar el juego --------------------
         if event.type == pygame.QUIT:
             juego.gameover = True
-    pygame.display.update()
+    update()
