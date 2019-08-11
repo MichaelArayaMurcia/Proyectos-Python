397432138021cdd4a2e223c78fd7ff571e4b64a0
Michael Araya <michaelarayamurcia@gmail.com>
2019-08-10 19:22:01 -0600

    Creando la comida

--- Snake/snake.py
+++ Snake/snake.py
@@ -6,15 +6,36 @@ class Juego:
 	def __init__(self):
 		self.largo = 400
 		self.ancho = 400
+		self.rojo = (255,0,0)
+		self.verde = (0,255,0)
 		self.gameover = False
 		self.screen = pygame.display.set_mode((self.largo,self.ancho))
 
 class Snake:
-	pass
+	def __init__(self):
+		self.x = 100
+		self.y = 100
+		self.largo = 10
+		self.ancho = 10
+	def show(self):
+		pygame.draw.rect(juego.screen,juego.verde,(self.x,self.y,self.largo,self.ancho))
+
+class Comida:
+	def __init__(self):
+		self.x = 120
+		self.y = 120
+		self.largo = 10
+		self.ancho = 10
+	def show(self):
+		pygame.draw.rect(juego.screen,juego.rojo,(self.x,self.y,self.largo,self.ancho))
 
 juego = Juego()
+snake = Snake()
+comida = Comida()
 
 def update():
+	snake.show()
+	comida.show()
 	pygame.display.update()
 
 
@@ -22,3 +43,4 @@ while juego.gameover != True:
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			juego.gameover = True
+	update()
