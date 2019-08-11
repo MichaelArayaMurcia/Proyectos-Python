cbfdfa1ae868a21fe681e5fd99636187a1327188
Michael Araya <michaelarayamurcia@gmail.com>
2019-08-10 20:02:41 -0600

    Añadiendo el fondo de pantalla y movimiento

--- Snake/snake.py
+++ Snake/snake.py
@@ -8,9 +8,14 @@ class Juego:
 		self.ancho = 400
 		self.rojo = (255,0,0)
 		self.verde = (0,255,0)
+		self.speedx = 0
+		self.speedy = 0
+		self.img = pygame.image.load('purple.png')
 		self.gameover = False
 		self.screen = pygame.display.set_mode((self.largo,self.ancho))
-
+	def show(self):
+		self.img = pygame.transform.scale(self.img,(self.largo,self.ancho))
+		self.screen.blit(self.img,(0,0))	
 class Snake:
 	def __init__(self):
 		self.x = 100
@@ -19,6 +24,9 @@ class Snake:
 		self.ancho = 10
 	def show(self):
 		pygame.draw.rect(juego.screen,juego.verde,(self.x,self.y,self.largo,self.ancho))
+	def update(self):
+		self.x += juego.speedx
+		self.y += juego.speedy
 
 class Comida:
 	def __init__(self):
@@ -34,13 +42,32 @@ snake = Snake()
 comida = Comida()
 
 def update():
+	juego.show()
 	snake.show()
+	snake.update()
 	comida.show()
 	pygame.display.update()
 
+def moverse():
+	if keys[pygame.K_RIGHT]:
+		juego.speedx = 1
+		juego.speedy = 0
+	elif keys[pygame.K_LEFT]:
+		juego.speedx = -1
+		juego.speedy = 0
+	elif keys[pygame.K_UP]:
+		juego.speedy = -1
+		juego.speedx = 0
+	elif keys[pygame.K_DOWN]:
+		juego.speedy = 1
+		juego.speedx = 0
 
 while juego.gameover != True:
+	keys = pygame.key.get_pressed()
 	for event in pygame.event.get():
 		if event.type == pygame.QUIT:
 			juego.gameover = True
+	moverse()
 	update()
+
+#--------------------- Final -------------------
